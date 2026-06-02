ken-jeong/winsock

feat: initial commit

midterm/ch01_tcp-server.c
@@ -1,0 +1,75 @@
+// gcc midterm/ch01_tcp-server.c -o midterm/ch01_tcp-server.exe -lws2_32
+
+// TCP 서버 프로그램
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <stdio.h>
+// #include <ws2tcpip.h>
+#include <WinSock2.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+#define  MAX_BUF_SIZE  1000
+
+// 에러 처리 함수 선언
+void ErrorHandling(char* message);
+
+int main(void) {
+	printf("> start TCP server.\n");
+
+	// 윈속 초기화
+	WSADATA winsockData;
+	WSAStartup(MAKEWORD(2,2), &winsockData);
+
+	// 서버 소켓 생성
+	SOCKET serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	// 서버 주소 설정
+	SOCKADDR_IN serverAddr;
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family		= AF_INET;
+	serverAddr.sin_port			= htons(9000); // 포트 번호 9000
+	serverAddr.sin_addr.s_addr	= inet_addr("127.0.0.1"); // 루프백 주소
+	// InetPton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+	// 소켓에 IP 주소와 포트 번호 할당
+	bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+
+	// 연결 요청 대기 상태로 전환 (대기 큐 크기: 5)
+	int backLog = 5; 
+	listen(serverSocket, backLog);
+
+	// 클라이언트 연결 요청 수락
+	SOCKADDR_IN clientAddr;
+	int addrLen = sizeof(clientAddr);
+	SOCKET clientSocket = accept(serverSocket, (SOCKADDR*)&clientAddr, &addrLen);
+
+	// 데이터 수신 루프
+	int stopFlag = 0;
+	char Buffer[MAX_BUF_SIZE];
+	while(stopFlag == 0) {
+		int returnValue = recv(clientSocket, Buffer, MAX_BUF_SIZE, 0);
+		if (returnValue > 0) {
+			// 데이터 수신 성공 시 수신한 문자열 출력
+			Buffer[returnValue] = '\0';
+			printf("> recv: %s", Buffer);
+		} else if (returnValue == SOCKET_ERROR) {
+			// 에러 발생 시 처리
+			ErrorHandling("recv() error");
+		} else if (returnValue == 0) {
+			// 클라이언트가 연결을 종료한 경우 루프 종료
+			stopFlag = 1;
+		}
+	}
+
+	// 소켓 및 윈속 리소스 해제
+	closesocket(serverSocket);
+	WSACleanup();
+	return 0;
+}
+
+// 에러 메시지 출력 후 프로그램 종료 함수
+void ErrorHandling(char* message) {
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}

midterm/ch02_tcp-client.c
@@ -1,0 +1,66 @@
+// gcc midterm/ch02_tcp-client.c -o midterm/ch02_tcp-client.exe -lws2_32
+
+// 클라이언트 프로그램
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+#define MAX_BUF_SIZE 100
+
+void ErrorHandling(char* message);
+
+int main(void) {
+	printf("> Client started.\n");
+
+	// 1. 윈속 라이브러리 초기화
+	WSADATA winsockData;
+	int errorNum = WSAStartup(MAKEWORD(2,2), &winsockData);
+	if (errorNum != 0)
+		ErrorHandling("WSAStartup() error!");
+
+	// 2. 소켓 생성 (IPv4 주소 체계, TCP)
+	SOCKET clientSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (clientSocket == INVALID_SOCKET)
+		ErrorHandling("TCP socket creation error");
+
+	SOCKADDR_IN  serverAddr;
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(9000);  
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	// 서버에 연결 요청
+	errorNum = connect(clientSocket, (SOCKADDR *)&serverAddr, sizeof(serverAddr) );
+	printf("> Client requested a TCP connection.\n");
+
+	// 반복해서 send() 함수를 호출하여 서버로 데이터를 전송
+	int index;
+	char Buffer[MAX_BUF_SIZE];
+
+	// 전송할 데이터 준비
+	for (index = 0; index < MAX_BUF_SIZE; index++) {
+		Buffer[index] = index;
+	}
+
+	for (index = 0; index < 5; index++ ) {
+		int returnValue = send(clientSocket, Buffer, MAX_BUF_SIZE, 0);
+		if (returnValue == SOCKET_ERROR) {
+			printf("<error> Error occurred during send(). code(%d)\n", WSAGetLastError());
+		} else if (returnValue > 0) {
+			printf("%d> Sent %d bytes via send().\n", index+1, returnValue );
+		}
+		//
+		Sleep(1000);  
+	}
+	errorNum = closesocket(clientSocket); // 소켓 닫기
+	errorNum = WSACleanup(); // 윈속 리소스 정리(해제)
+}
+
+void ErrorHandling(char* message) {
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}

ken-jeong/socket

midterm/ch01_tcp-server.c
@@ -1,0 +1,73 @@
+// gcc midterm/ch01_tcp-server.c -o midterm/ch01_tcp-server.exe -lws2_32
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <stdio.h>
+// #include <ws2tcpip.h>
+#include <WinSock2.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+#define  MAX_BUF_SIZE 1000
+
+// 에러 처리 함수 선언
+void ErrorHandling(char* message);
+
+int main(void) {
+    printf("> start TCP server.\n");
+
+    WSADATA wsadata;
+    WSAStartup(MAKEWORD(2,2), &wsadata); // Winsock 라이브러리 초기화
+
+    // 서버 소켓 생성
+    SOCKET serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    // 서버 주소 설정
+    SOCKADDR_IN serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family        = AF_INET;
+    serverAddr.sin_port            = htons(9000); // 포트 번호 9000
+    serverAddr.sin_addr.s_addr    = inet_addr("127.0.0.1"); // 루프백 주소
+    // InetPton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+    // 소켓에 IP 주소와 포트 번호 할당
+    bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+
+    // 연결 요청 대기 상태로 전환 (대기 큐 크기: 5)
+    int backLog = 5;
+    listen(serverSocket, backLog);
+
+    // 클라이언트 연결 요청 수락
+    SOCKADDR_IN clientAddr;
+    int addrLen = sizeof(clientAddr);
+    SOCKET clientSocket = accept(serverSocket, (SOCKADDR*)&clientAddr, &addrLen);
+
+    // 데이터 수신 루프
+    int stopFlag = 0;
+    char Buffer[MAX_BUF_SIZE];
+    while (stopFlag == 0) {
+        int returnValue = recv(clientSocket, Buffer, MAX_BUF_SIZE, 0);
+        if (returnValue > 0) {
+            // 데이터 수신 성공 시 수신한 문자열 출력
+            Buffer[returnValue] = '\0';
+            printf("> recv: %s", Buffer);
+        } else if (returnValue == SOCKET_ERROR) {
+            // 에러 발생 시 처리
+            ErrorHandling("recv() error");
+        } else if (returnValue == 0) {
+            // 클라이언트가 연결을 종료한 경우 루프 종료
+            stopFlag = 1;
+        }
+    }
+
+    // 소켓 및 윈속 리소스 해제
+    closesocket(serverSocket);
+    WSACleanup();
+    return 0;
+}
+
+// 에러 메시지 출력 후 프로그램 종료 함수
+void ErrorHandling(char* message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}

midterm/ch02_tcp-client.c
@@ -1,0 +1,65 @@
+// gcc midterm/ch02_tcp-client.c -o midterm/ch02_tcp-client.exe -lws2_32
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+#define MAX_BUF_SIZE 100
+
+void ErrorHandling(char* message);
+
+int main(void) {
+    printf("> Client started.\n");
+
+    WSADATA wsadata;
+    int result = WSAStartup(MAKEWORD(2,2), &wsadata);
+    if (result != 0) {
+        ErrorHandling("WSAStartup() error!");
+    }
+
+    // 소켓 생성 (IPv4 주소 체계, TCP)
+    SOCKET clientSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSocket == INVALID_SOCKET)
+        ErrorHandling("TCP socket creation error");
+
+    SOCKADDR_IN  serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(9000);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // 서버에 연결 요청
+    result = connect(clientSocket, (SOCKADDR *)&serverAddr, sizeof(serverAddr) );
+    printf("> Client requested a TCP connection.\n");
+
+    // 반복해서 send() 함수를 호출하여 서버로 데이터를 전송
+    int index;
+    char Buffer[MAX_BUF_SIZE];
+
+    // 전송할 데이터 준비
+    for (index = 0; index < MAX_BUF_SIZE; index++) {
+        Buffer[index] = index;
+    }
+
+    for (index = 0; index < 5; index++ ) {
+        int returnValue = send(clientSocket, Buffer, MAX_BUF_SIZE, 0);
+        if (returnValue == SOCKET_ERROR) {
+            printf("<error> Error occurred during send(). code(%d)\n", WSAGetLastError());
+        } else if (returnValue > 0) {
+            printf("%d> Sent %d bytes via send().\n", index+1, returnValue );
+        }
+        //
+        Sleep(1000);
+    }
+    result = closesocket(clientSocket); // 소켓 닫기
+    result = WSACleanup(); // 윈속 리소스 정리(해제)
+}
+
+void ErrorHandling(char* message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}